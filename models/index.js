007c369caa1ffffab9200bcd0aaa31308f76d787
spurgason <shanepurgason.98@gmail.com>
2021-10-21 16:01:42 -0400

    added throughs and foreign keys

--- models/index.js
+++ models/index.js
@@ -5,16 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag
+  through: ProductTag,
+  as: 'product_tag',
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
