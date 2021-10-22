cc3a7af360edefbfcaad33939dffdd959d457779
spurgason <shanepurgason.98@gmail.com>
2021-10-21 22:03:19 -0400

    all routes work as intended

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,7 +21,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
+  // find one category by its `id` value 
   // be sure to include its associated Products
   Category.findOne({
     where: {
